68f611ca2df6cab668c10859688bd81104edd988
58381 <najoua.jabour@isfsc.be>
2023-11-11 00:39:03 +0100

    Etape 1

--- Front end/jquery/js/script.js
+++ Front end/jquery/js/script.js
@@ -7,5 +7,12 @@ $(function() {
     2. affiche l'image adéquate
     3. affiche le paragraphe adéquat
     */
-    
+    $('.js-links a').on('click', function(e) {
+        // 1. Mettre en avant le lien sur lequel on clique :
+        e.preventDefault(); // Pour pas que le click aille n'importe ou
+        $('.js-links a').removeClass('active'); // Empêche que tout soit cliqué en même temps
+        $(this).addClass('active'); // Ajouter la classe 'active' au lien sur lequel on a clique
+
+    });
+
 });
