f2bf18a21e4fa2850e898e1beea4acc57febb0db
58381 <najoua.jabour@isfsc.be>
2023-11-11 23:21:37 +0100

    Correction
    De tous les défauts du point 1 et 2

--- Front end/jquery/js/script.js
+++ Front end/jquery/js/script.js
@@ -1,13 +1,5 @@
 // cette ligne permet d'être sûr que le document est prêt à utiliser jQuery
 $(function() {
-    /*  
-    Quand je clique sur un élément de la navigation, 
-    le composant : 
-    1. met en avant le lien sur lequel je viens de cliquer
-    2. affiche l'image adéquate
-    3. affiche le paragraphe adéquat
-    */
-
    // 1. met en avant le lien sur lequel je viens de cliquer :
     $('.js-links a').on('click', function(e) {
         e.preventDefault(); // Pour pas que le click aille n'importe ou
@@ -25,17 +17,14 @@ display_image(imageUrl, index);
 
 });
  // Afficher l'image adéquat :
- function display_image(src, index) {
+ function display_image(src) {
     var img = document.createElement("img");
     img.src = src;
-    img.alt = 'Étape ' + index;
     
     // Effacer les images non adéquat
     $('.js-images').html('');
     $('.js-images').append(img);
 }
-
    // 3. Affiche le paragraphe adéquat :
-   
 
 });
