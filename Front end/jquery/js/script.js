5d1354a2750723fa0f278232cbb10dc1c6c18ab0
58381 <najoua.jabour@isfsc.be>
2023-11-11 23:48:34 +0100

    Correction point 2

--- Front end/jquery/js/script.js
+++ Front end/jquery/js/script.js
@@ -1,30 +1,36 @@
 // cette ligne permet d'être sûr que le document est prêt à utiliser jQuery
 $(function() {
-   // 1. met en avant le lien sur lequel je viens de cliquer :
+    // 1. Mettre en avant le lien sur lequel je viens de cliquer :
     $('.js-links a').on('click', function(e) {
-        e.preventDefault(); // Pour pas que le click aille n'importe ou
-        $('.js-links a').removeClass('active'); // Empêche que tout soit cliqué en même temps
-        $(this).addClass('active'); // Ajouter la classe 'active' au lien sur lequel on a clique
-    
+        e.preventDefault(); // Pour éviter que le clic aille n'importe où
+        $('.js-links a').removeClass('active'); // Empêcher que tout soit cliqué en même temps
+        $(this).addClass('active'); // Ajouter la classe 'active' au lien sur lequel on a cliqué
 
-    // 2. Afficher l'image adéquat :
-// Récupérer l'index du lien et afficher l'image adéquat
-var index = $(this).attr('data-index');
-var imageUrl = 'https://place-hold.it/200x300/DDD/000/?text=' + index + '&bold&fontsize=34';
+        // 2. Afficher l'image adéquate :
+        // Récupérer l'index du lien
+        var index = $(this).attr('data-index');
+        
+        // Récupérer l'attribut 'src' de l'image correspondante
+        var src = $('.js-images img[data-index="' + index + '"]').attr('src');
 
-// Appeler la fonction pour afficher l'image
-display_image(imageUrl, index);
+        // Appeler la fonction pour afficher l'image
+        display_image(src);
+    });
 
+    // 3. Afficher le paragraphe adéquat :
 });
- // Afficher l'image adéquat :
- function display_image(src) {
+
+// Fonction pour afficher l'image adéquate
+function display_image(src) {
+    // Créer un élément img
     var img = document.createElement("img");
-    img.src = src;
     
-    // Effacer les images non adéquat
+    // Mettre l'attribut 'src' en tant que source de l'image
+    img.src = src;
+
+    // Effacer les images non adéquates
     $('.js-images').html('');
+    
+    // Ajouter l'image à l'élément avec la classe 'js-images'
     $('.js-images').append(img);
 }
-   // 3. Affiche le paragraphe adéquat :
-
-});
