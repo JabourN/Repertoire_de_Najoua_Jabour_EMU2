116e6d6f95f1f39b8be5ae7372a8bd773b35026d
58381 <najoua.jabour@isfsc.be>
2023-11-11 22:29:28 +0100

    Correction du point 2

--- Front end/jquery/js/script.js
+++ Front end/jquery/js/script.js
@@ -8,42 +8,34 @@ $(function() {
     3. affiche le paragraphe adéquat
     */
 
-   // 1. Mettre en avant le lien sur lequel on clique :
+   // 1. met en avant le lien sur lequel je viens de cliquer :
     $('.js-links a').on('click', function(e) {
         e.preventDefault(); // Pour pas que le click aille n'importe ou
         $('.js-links a').removeClass('active'); // Empêche que tout soit cliqué en même temps
         $(this).addClass('active'); // Ajouter la classe 'active' au lien sur lequel on a clique
-    });
+    
 
     // 2. Afficher l'image adéquat :
-    function display_image(src, img) {
-        var img = document.createElement("img");
-        img.src = src;
+// Récupérer l'index du lien et afficher l'image adéquat
+var index = $(this).attr('data-index');
+var imageUrl = 'https://place-hold.it/200x300/DDD/000/?text=' + index + '&bold&fontsize=34';
 
-        // Effacez le contenu du body avant d'ajouter la nouvelle image
-        document.body.innerHTML = '';
-        document.body.appendChild(img);
-    }
+// Appeler la fonction pour afficher l'image
+display_image(imageUrl, index);
 
-    // Fonction pour gérer le clic sur les liens
-    function handleLinkClick(event) {
-        // Empêcher le comportement par défaut du lien
-        event.preventDefault();
+});
+ // Afficher l'image adéquat :
+ function display_image(src, index) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.alt = 'Étape ' + index;
+    
+    // Effacer les images non adéquat
+    $('.js-images').html('');
+    $('.js-images').append(img);
+}
 
-        // Récupérer l'index du lien
-        var index = event.target.getAttribute('data-index');
-
-        // Construire l'URL de l'image en fonction de l'index !!!FAIL!!!
-        var imageUrl = 'https://place-hold.it/200x300/DDD/000/?text='+ index + '&bold&fontsize=34';
-        
-        // Appeler tous les paramètres précédent
-        display_image(imageUrl, 200, 300 + index );
-    }
-
-    // Ajouter un gestionnaire d'événements à chaque lien
-    var links = document.querySelectorAll('.nav-link');
-    links.forEach(function(link) {
-        link.addEventListener('click', handleLinkClick);
-    });
+   // 3. Affiche le paragraphe adéquat :
+   
 
 });
